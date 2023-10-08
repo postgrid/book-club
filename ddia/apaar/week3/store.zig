462f4df144f79feaf630d3079da686088534b440
Apaar Madan <apaar_madan@hotmail.com>
2023-10-08 16:51:23 -0400

    Persistence to the Zig KV store

--- ddia/apaar/week3/store.zig
+++ ddia/apaar/week3/store.zig
@@ -55,7 +55,6 @@ const Store = struct {
     const Self = @This();
 
     pub fn init(allocator: Allocator, pathname: []const u8) !Self {
-        // TODO(Apaar): Initialize the map with the key and value offsets
         const segment_file = try std.fs.cwd().createFile(
             pathname,
             .{
@@ -63,13 +62,90 @@ const Store = struct {
                 .read = true,
             },
         );
+        errdefer {
+            segment_file.close();
+        }
+
+        var key_to_value_metadata = KeyToValueMetadataMap.init(allocator);
+        errdefer {
+            // TODO(Apaar): Free keys
+            key_to_value_metadata.deinit();
+        }
+
+        // TODO(Apaar): Initialize the map with the key and value offsets
+        var op_type_key_len_buf: [1 + 8]u8 = undefined;
+
+        var temp_key_buf = std.ArrayList(u8).init(allocator);
+        defer temp_key_buf.deinit();
+
+        while (true) {
+            const count = try segment_file.readAll(&op_type_key_len_buf);
+            if (count == 0) {
+                break;
+            }
+
+            const op_type: WriteOpType = @enumFromInt(op_type_key_len_buf[0]);
+
+            var key_len_buf: [8]u8 align(@alignOf(u64)) = undefined;
+            @memcpy(&key_len_buf, op_type_key_len_buf[1..]);
+
+            const key_len: u64 = @bitCast(key_len_buf);
+
+            switch (op_type) {
+                .set => {
+                    var value_len_buf: [8]u8 align(@alignOf(u64)) = undefined;
+
+                    _ = try segment_file.readAll(&value_len_buf);
+
+                    const value_len: u64 = @bitCast(value_len_buf);
+
+                    // Read in the key
+                    try temp_key_buf.resize(key_len);
+
+                    _ = try segment_file.readAll(temp_key_buf.items);
+
+                    const value_metadata = ValueMetadata{
+                        // We should be at the value now since we read in the key above
+                        .offset = try segment_file.getPos(),
+                        .len = value_len,
+                    };
+
+                    // Skip over the value
+                    _ = try segment_file.seekBy(@intCast(value_len));
+
+                    const entry = try key_to_value_metadata.getOrPutAdapted(
+                        @as([]const u8, temp_key_buf.items),
+                        key_to_value_metadata.ctx,
+                    );
+
+                    if (!entry.found_existing) {
+                        // Copy it into our own non-temp buffer under our allocator
+                        const key_buf = try allocator.alloc(u8, key_len);
+                        @memcpy(key_buf, temp_key_buf.items);
+
+                        entry.key_ptr.* = key_buf;
+                    }
+
+                    entry.value_ptr.* = value_metadata;
+                },
+
+                .del => {
+                    const removed = key_to_value_metadata.fetchRemove(temp_key_buf.items);
+
+                    if (removed) |entry| {
+                        // TODO(Apaar): Do not assume we always own the keys
+                        allocator.free(entry.key);
+                    }
+                },
+            }
+        }
 
         try segment_file.seekTo(try segment_file.getEndPos());
 
         return .{
             .allocator = allocator,
             .segment_file = segment_file,
-            .key_to_value_metadata = KeyToValueMetadataMap.init(allocator),
+            .key_to_value_metadata = key_to_value_metadata,
         };
     }
 
@@ -80,6 +156,7 @@ const Store = struct {
         }
 
         self.key_to_value_metadata.deinit();
+        self.segment_file.close();
     }
 
     pub fn get(self: *Self, key: []const u8) ?ValueProxy {
@@ -123,13 +200,27 @@ const Store = struct {
 
         try self.segment_file.writevAll(&iovecs);
 
-        const key_copy = try self.allocator.alloc(u8, key.len);
-        @memcpy(key_copy, key);
-
-        try self.key_to_value_metadata.put(key_copy, ValueMetadata{
+        const value_metadata = ValueMetadata{
             .offset = pos + op_type_buf.len + key_len_buf.len + value_len_buf.len + key.len,
             .len = value.len,
-        });
+        };
+
+        // TODO(Apaar): Idk why I can have a .{} here for the ctx but if I try to do the same thing in the `init` function
+        // it complains that my context is incomplete.
+        //
+        // Compiler bug?
+        //
+        // Nvm it's complaining again???? Oh it was eliminating the dead code of this function.
+        const entry = try self.key_to_value_metadata.getOrPutAdapted(key, self.key_to_value_metadata.ctx);
+
+        if (!entry.found_existing) {
+            var key_copy = try self.allocator.alloc(u8, key.len);
+            @memcpy(key_copy, key);
+
+            entry.key_ptr.* = key_copy;
+        }
+
+        entry.value_ptr.* = value_metadata;
     }
 
     pub fn del(self: *Self, key: []const u8) !bool {
@@ -171,6 +262,7 @@ pub fn main() !void {
     defer store.deinit();
 
     try store.setAllocKey("hello", "world");
+    try store.setAllocKey("goodbye", "universe");
 
     const value = store.get("hello");
 
