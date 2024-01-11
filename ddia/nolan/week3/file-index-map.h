621b890525bc242f53f61a4dc63f220e5d357d64
NolanPyottPG <nolan@postgrid.com>
2024-01-11 01:38:39 -0500

    Nolan Week 3 Update - Add support for basic hashmap - Try new error handling using `goto` - Every parameter with resources is a pointer

--- ddia/nolan/week3/file-index-map.h
+++ ddia/nolan/week3/file-index-map.h
@@ -2,6 +2,7 @@
 #define FILE_INDEX_MAP_H_
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct Buffer
 {
@@ -15,13 +16,33 @@ typedef struct FileIndex
     size_t len;
 } FileIndex;
 
+typedef struct FileIndexNode
+{
+    char *key;
+    FileIndex fileIndex;
+} FileIndexNode;
+
+typedef struct FileIndexBucket
+{
+    size_t nodeCount;
+    size_t nodeCapacity;
+    FileIndexNode *nodes;
+} FileIndexBucket;
+
 // FIXME
 typedef struct FileIndexHashMap
 {
+    size_t size;
+    size_t bucketCount;
+
+    FileIndexBucket *buckets;
 } FileIndexHashMap;
 
-void FileIndexSet(FileIndexHashMap map, char *key, FileIndex index);
-FileIndex FileIndexGet(FileIndexHashMap map, char *key);
-void FileIndexDelete(FileIndexHashMap map, char *key);
+FileIndexHashMap *FileIndexCreate();
+void FileIndexClose(FileIndexHashMap *map);
+
+void FileIndexSet(FileIndexHashMap *map, char *key, FileIndex index);
+FileIndex FileIndexGet(FileIndexHashMap *map, char *key, bool *found);
+void FileIndexDelete(FileIndexHashMap *map, char *key);
 
 #endif
